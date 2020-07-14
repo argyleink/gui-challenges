c94b2c65053cb7148803b3e88afad8f4ce6e6b96
Adam Argyle <argyle@google.com>
2020-07-14 15:59:43 -0700

    js todo's completed

--- sidenav/app/js/index.js
+++ sidenav/app/js/index.js
@@ -1,4 +1,17 @@
-// todo:
-// - ESC should close the menu
-// - Focus should be on the close button when opened
-// - Focus should go back to the menu when closed
+const sidenav = document.querySelector('#sidenav-open')
+const closenav = document.querySelector('#sidenav-close')
+const hamburger = document.querySelector('#sidenav-button')
+
+// set focus to our open/close buttons after animation
+sidenav.addEventListener('transitionend', e => {
+  if (document.location.hash === '#sidenav-open')
+    closenav.focus()
+  else
+    hamburger.focus()
+})
+
+// close our menu when esc is pressed
+sidenav.addEventListener('keyup', e => {
+  if (e.code === 'Escape')
+    document.location.hash = ''
+})
