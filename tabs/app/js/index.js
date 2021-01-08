35f61e04d08f291bb5431178dd6b1d501b60d4be
Adam Argyle <argyle@google.com>
2021-01-08 14:37:02 -0800

    some polish

--- tabs/app/js/index.js
+++ tabs/app/js/index.js
@@ -1,19 +1,24 @@
 import 'https://argyleink.github.io/scroll-timeline/dist/scroll-timeline.js'
 
+// grab and stash elements
 const tabgroup     = document.querySelector('.tabs')
 const tabsection   = tabgroup.querySelector(':scope > section')
 const tabnav       = tabgroup.querySelector(':scope > nav')
 const tabnavitems  = tabgroup.querySelectorAll(':scope > nav a')
 const tabindicator = tabgroup.querySelector(':scope .indicator')
 
+// based on the scroll timeline
+// move the indicator left/right based on the nav link left
+// resize the indicator width based on the nav link intrinsic size
+// NOTE: this can't be done in CSS
 tabindicator.animate(
   { 
     transform: [...tabnavitems].map(item => 
       `translateX(${item.offsetLeft}px)`),
     width: [...tabnavitems].map(item => 
-      `${item.offsetWidth}px`),
+      `${item.offsetWidth}px`)
   },
-  { 
+  {
     duration: 1000,
     fill: 'both',
     timeline: new ScrollTimeline({
@@ -23,6 +28,30 @@ tabindicator.animate(
     })
   }
 )
+
+// for each nav link
+// animate color based on the scroll timeline
+// color is active when its the current index
+// NOTE: this could be done in CSS
+tabnavitems.forEach(navitem => {
+  navitem.animate(
+    {
+      color: [...tabnavitems].map(item => 
+        item === navitem
+          ? `var(--text-active-color)`
+          : `var(--text-color)`)
+    },
+    {
+      duration: 1000,
+      fill: 'both',
+      timeline: new ScrollTimeline({
+        scrollSource: tabsection,
+        orientation: 'inline',
+        fill: 'both',
+      })
+    }
+  )
+})
 
 const setActiveTab = tabbtn => {
   document
