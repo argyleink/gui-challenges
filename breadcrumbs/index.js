5d6c23002c04aa88b20e1913408fa20723baec73
Adam Argyle <argyle@google.com>
2021-06-11 10:49:06 -0700

    adds roving index

--- breadcrumbs/index.js
+++ breadcrumbs/index.js
@@ -1,15 +1,27 @@
-const breadcrumbs = document.querySelector('.breadcrumbs')
-const navs = document.querySelectorAll('.breadcrumbs select')
-const allowedKeys = new Set(['Tab', 'Enter', ' '])
-const preventedKeys = new Set(['ArrowUp', 'ArrowDown'])
+import {rovingIndex} from 'roving-ux'
 
+const breadcrumbs    = document.querySelectorAll('.breadcrumbs')
+const navs           = document.querySelectorAll('.breadcrumbs select')
+const allowedKeys    = new Set(['Tab', 'Enter', ' '])
+const preventedKeys  = new Set(['ArrowUp', 'ArrowDown'])
+
+// roving UX for breadcrumb components
+breadcrumbs.forEach(nav => 
+  rovingIndex({
+    element: nav,
+    target: 'a,select',
+  }))
+
+// watch crumbs for changes,
+// ensures it's a full value change, 
+// not a user exploring options via keyboard
 navs.forEach(nav => {
   let ignoreChange = false
 
   nav.addEventListener('change', e => {
     if (ignoreChange) return
 
-    console.info('User selected', e.target.value)
+    console.info('User wishes to change path to: ', e.target.value)
     // location.pathname = target.value
   })
 
