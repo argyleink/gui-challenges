5f17d53b545faa043120559e976ba32d5df1113a
Adam Argyle <argyle@google.com>
2021-06-21 15:56:54 -0700

    remove roving ux

--- breadcrumbs/index.js
+++ breadcrumbs/index.js
@@ -1,16 +1,8 @@
-import {rovingIndex} from 'roving-ux'
-
 const breadcrumbs    = document.querySelectorAll('.breadcrumbs')
 const navs           = document.querySelectorAll('.breadcrumbs select')
+
 const allowedKeys    = new Set(['Tab', 'Enter', ' '])
 const preventedKeys  = new Set(['ArrowUp', 'ArrowDown'])
-
-// roving UX for breadcrumb components
-breadcrumbs.forEach(nav => 
-  rovingIndex({
-    element: nav,
-    target: 'a,select',
-  }))
 
 // watch crumbs for changes,
 // ensures it's a full value change, 
@@ -25,10 +17,8 @@ navs.forEach(nav => {
     const choice = option.value
 
     option.closest('.crumb').querySelector(':scope > a').textContent = choice
-    console.info('User wishes to change path to: ', choice)
-
+    console.info('change path to: ', choice)
     // change entire URL
-    // location.pathname = target.value
     // or 
     // use your favorite clientside framework's router
   })
