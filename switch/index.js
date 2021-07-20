4067d32b327fec36e9b139e9f51005382f65c85e
Adam Argyle <argyle@google.com>
2021-07-20 11:18:05 -0700

    cleanup and perfect the ux

--- switch/index.js
+++ switch/index.js
@@ -1,20 +1,8 @@
-const switches = document.querySelectorAll('.gui-switch')
+const elements = document.querySelectorAll('.gui-switch')
+const switches = new Map()
 
 const state = {
   activethumb: null,
-}
-
-const dragging = event => {
-  let thumbsize = getPseudoSize(state.activethumb, 'width')
-  let lowerBoundary = 0
-  let upperBoundary = state.activethumb.clientWidth - thumbsize - 4
-  let pos = event.offsetX - thumbsize / 2
-
-  if (pos < lowerBoundary) pos = 0
-  if (pos > upperBoundary) pos = upperBoundary
-
-  state.activethumb.style.setProperty('--thumb-transition-duration', '0s')
-  state.activethumb.style.setProperty('--thumb-position', `${pos}px`)
 }
 
 const dragInit = event => {
@@ -22,13 +10,21 @@ const dragInit = event => {
   event.target.addEventListener('pointermove', dragging)
 }
 
-const dragEnd = event => {
+const dragging = event => {
   if (!state.activethumb) return
 
-  let curpos = parseInt(state.activethumb.style.getPropertyValue('--thumb-position'))
+  let swx = switches.get(state.activethumb.closest('.gui-switch'))
+  let pos = event.offsetX - swx.thumbsize / 2
 
-  if (curpos >= 32)
-    state.activethumb.checked = true
+  if (pos < swx.bounds.lower) pos = 0
+  if (pos > swx.bounds.upper) pos = swx.bounds.upper
+
+  state.activethumb.style.setProperty('--thumb-transition-duration', '0s')
+  state.activethumb.style.setProperty('--thumb-position', `${pos}px`)
+}
+
+const dragEnd = event => {
+  if (!state.activethumb) return
 
   state.activethumb.style.removeProperty('--thumb-position')
   state.activethumb.style.removeProperty('--thumb-transition-duration')
@@ -36,16 +32,51 @@ const dragEnd = event => {
   state.activethumb = null
 }
 
-const getPseudoSize = (element, prop) => {
+const getStyle = (element, prop) => {
+  return parseInt(
+    window.getComputedStyle(element)
+      .getPropertyValue(prop)
+  )
+}
+
+const getPseudoStyle = (element, prop) => {
   return parseInt(
     window.getComputedStyle(element, ':before')
       .getPropertyValue(prop)
   )
 }
 
-switches.forEach(s => {
-  s.addEventListener('pointerdown', dragInit)
-  s.addEventListener('pointerup', dragEnd)
+elements.forEach(sx => {
+  sx.addEventListener('pointerdown', dragInit)
+  sx.addEventListener('pointerup', dragEnd)
+
+  let checkbox = sx.querySelector('input')
+
+  let thumbsize = getPseudoStyle(checkbox, 'width')
+  let padding = getStyle(checkbox, 'padding-left') + getStyle(checkbox, 'padding-right')
+
+  switches.set(sx, {
+    thumbsize,
+    padding,
+    bounds: {
+      lower: 0,
+      middle: (checkbox.clientWidth - padding) / 4,
+      upper: checkbox.clientWidth - thumbsize - padding,
+    },
+  })
 })
 
-window.addEventListener('pointerup', dragEnd)
+window.addEventListener('pointerup', event => {
+  if (!state.activethumb) return
+
+  let sx = switches.get(state.activethumb.parentElement)
+
+  let curpos = parseInt(
+    state.activethumb.style.getPropertyValue('--thumb-position')
+  )
+
+  if (curpos >= sx.bounds.middle) 
+    state.activethumb.checked = true
+
+  dragEnd(event)
+})
