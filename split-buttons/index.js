40c5124427c24444ba78f2368df5a194be9541b7
Adam Argyle <argyle@google.com>
2021-08-04 16:54:44 -0700

    watch for popup changes

--- split-buttons/index.js
+++ split-buttons/index.js
@@ -5,3 +5,11 @@ document.querySelectorAll('.gui-split-button > span')
     element,
     target: 'button',
   }))
+
+document.querySelectorAll('.gui-popup')
+  .forEach(popup => {
+    popup.addEventListener('click', event => {
+      let splitbtn = event.target.closest('.gui-split-button')
+      splitbtn.querySelector(':scope > button').textContent = event.target.innerText
+    })
+  })
