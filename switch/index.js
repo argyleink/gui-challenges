7beca7f712cac1a8180e9d5ac79b4f93378e37d4
Chris Kruining <chris@kruining.eu>
2021-08-12 02:07:22 +0200

    code as custom element

--- switch/index.js
+++ switch/index.js
@@ -1,119 +1,144 @@
 import {getStyle, getPseudoStyle} from './style-utils.js'
 
-const elements = document.querySelectorAll('.gui-switch')
-const switches = new WeakMap()
+class Switch extends HTMLInputElement
+{
+	_isDragging = false;
+  _recentlyDragged = false;
+  _thumbsize = 0;
+  _padding = 0;
+  _bounds = {
+    lower: 0,
+    middle: 0,
+    upper: 0,
+  };
+  
+  connectedCallback()
+  {
+  	// There is some racing condition, no clue as to what, this delay seems to circumvent it for now
+  	setTimeout(_ => {
+      const thumbsize = getPseudoStyle(this, 'width');
+      const padding = getStyle(this, 'padding-left') + getStyle(this, 'padding-right');
 
-const state = {
-  activethumb: null,
-  recentlyDragged: false,
-}
+      this._thumbsize = thumbsize;
+      this._padding = padding;
+      this._bounds = {
+        lower: 0,
+        middle: (this.clientWidth - padding) / 4,
+        upper: this.clientWidth - thumbsize - padding,
+      };
 
-const dragInit = event => {
-  if (event.target.disabled) return
+      this.addEventListener('pointerdown', this.dragInit.bind(this));
+      this.addEventListener('pointerup', this.dragEnd.bind(this));
+      this.addEventListener('click', this.preventBlubbling.bind(this));
 
-  state.activethumb = event.target
-  state.activethumb.addEventListener('pointermove', dragging)
-  state.activethumb.style.setProperty('--thumb-transition-duration', '0s')
-}
-
-const dragging = event => {
-  if (!state.activethumb) return
-
-  let {thumbsize, bounds, padding} = switches.get(state.activethumb.parentElement)
-  let directionality = getStyle(state.activethumb, '--isLTR')
-
-  let track = (directionality === -1)
-    ? (state.activethumb.clientWidth * -1) + thumbsize + padding
-    : 0
-
-  let pos = Math.round(event.offsetX - thumbsize / 2)
-
-  if (pos < bounds.lower) pos = 0
-  if (pos > bounds.upper) pos = bounds.upper
-
-  state.activethumb.style.setProperty('--thumb-position', `${track + pos}px`)
-}
-
-const dragEnd = event => {
-  if (!state.activethumb) return
-
-  state.activethumb.checked = determineChecked()
-
-  if (state.activethumb.indeterminate)
-    state.activethumb.indeterminate = false
-
-  state.activethumb.style.removeProperty('--thumb-transition-duration')
-  state.activethumb.style.removeProperty('--thumb-position')
-  state.activethumb.removeEventListener('pointermove', dragging)
-  state.activethumb = null
-
-  padRelease()
-}
-
-const padRelease = () => {
-  state.recentlyDragged = true
-
-  setTimeout(_ => {
-    state.recentlyDragged = false
-  }, 300)
-}
-
-const preventBubbles = event => {
-  if (state.recentlyDragged)
-    event.preventDefault() && event.stopPropagation()
-}
-
-const labelClick = event => {
-  if (
-    state.recentlyDragged || 
-    !event.target.classList.contains('gui-switch') || 
-    event.target.querySelector('input').disabled
-  ) return
-
-  let checkbox = event.target.querySelector('input')
-  checkbox.checked = !checkbox.checked
-  event.preventDefault()
-}
-
-const determineChecked = () => {
-  let {bounds} = switches.get(state.activethumb.parentElement)
-  let curpos = 
-    Math.abs(
-      parseInt(
-        state.activethumb.style.getPropertyValue('--thumb-position')))
-
-  if (!curpos) {
-    curpos = state.activethumb.checked
-      ? bounds.lower
-      : bounds.upper
+      window.addEventListener('pointerup', this.dragEnd.bind(this));
+    }, 1000);
   }
+  
+  disconnectedCallback()
+  {
+    this.removeEventListener('pointerdown', this.dragInit.bind(this));
+    this.removeEventListener('pointerup', this.dragEnd.bind(this));
+    this.removeEventListener('click', this.preventBlubbling.bind(this));
+    
+    window.removeEventListener('pointerup', this.dragEnd.bind(this));
+  }
+  
+  dragInit()
+  {
+    if (this.disabled)
+    {
+    	return;
+    }
+    
+    this._isDragging = true;
+    
+    this.addEventListener('pointermove', this.dragging.bind(this));
+    this.style.setProperty('--thumb-transition-duration', '0s');
+  }
+  
+  dragEnd()
+  {
+  	if(this._isDragging !== true)
+    {
+    	return;
+    }
 
-  return curpos >= bounds.middle
+    this.checked = this.determineChecked();
+
+    if (this.indeterminate);
+    {
+    	this.indeterminate = false;
+    }
+
+    this.style.removeProperty('--thumb-transition-duration');
+    this.style.removeProperty('--thumb-position');
+    this.removeEventListener('pointermove', this.dragging.bind(this));
+    
+    this._isDragging = false;
+
+    this.padRelease();
+  }
+  
+  dragging(event)
+  {
+    if(this._isDragging !== true)
+    {
+    	return;
+    }
+
+    const directionality = getStyle(this, '--isLTR');
+    const track = (directionality === -1)
+      ? (this.clientWidth * -1) + this._thumbsize + this._padding
+      : 0;
+
+    let pos = Math.round(event.offsetX - this._thumbsize / 2);
+
+    if(pos < this._bounds.lower)
+    {
+    	pos = 0;
+    }
+    
+    if(pos > this._bounds.upper)
+    {
+    	pos = this._bounds.upper;
+    }
+
+    this.style.setProperty('--thumb-position', `${track + pos}px`);
+  }
+  
+  determineChecked()
+  {
+    let curpos = Math.abs(
+    	Number.parseInt(
+      	this.style.getPropertyValue('--thumb-position')
+      )
+    );
+
+    if (!curpos)
+    {
+      curpos = this.checked
+        ? this._bounds.lower
+        : this._bounds.upper;
+    }
+
+    return curpos >= this._bounds.middle;
+  }
+  
+  padRelease()
+  {
+    this._recentlyDragged = true;
+
+    setTimeout(_ => this._recentlyDragged = false, 300);
+  }
+  
+  preventBlubbling()
+  {
+  	if(this._recentlyDragged)
+  	{
+    	event.preventDefault() && event.stopPropagation();
+    }
+  }
 }
 
-elements.forEach(guiswitch => {
-  let checkbox = guiswitch.querySelector('input')
-  let thumbsize = getPseudoStyle(checkbox, 'width')
-  let padding = getStyle(checkbox, 'padding-left') + getStyle(checkbox, 'padding-right')
-
-  checkbox.addEventListener('pointerdown', dragInit)
-  checkbox.addEventListener('pointerup', dragEnd)
-  checkbox.addEventListener('click', preventBubbles)
-  guiswitch.addEventListener('click', labelClick)
-
-  switches.set(guiswitch, {
-    thumbsize,
-    padding,
-    bounds: {
-      lower: 0,
-      middle: (checkbox.clientWidth - padding) / 4,
-      upper: checkbox.clientWidth - thumbsize - padding,
-    },
-  })
-})
-
-window.addEventListener('pointerup', event => {
-  if (!state.activethumb) return
-
-  dragEnd(event)
-})
+customElements.define('argyle-switch', Switch, { extends: 'input' });
