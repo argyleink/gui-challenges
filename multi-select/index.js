b84b06bcd1bed3d22c5c2e9e87036782feea9465
Adam Argyle <argyle@google.com>
2021-08-27 11:14:44 -0700

    adds isotope visual filtering

--- multi-select/index.js
+++ multi-select/index.js
@@ -1,9 +1,24 @@
+import 'https://unpkg.com/isotope-layout@3.0.6/dist/isotope.pkgd.min.js'
+
+const IsotopeGrid = new Isotope( 'article', {
+  itemSelector: 'span',
+  layoutMode: 'fitRows',
+})
+
 document.querySelector('select').addEventListener('input', e => {
   let selectData = Array.from(e.target.selectedOptions).reduce((data, opt) => {
     data.push([opt.parentElement.label, opt.value])
     return data
   }, [])
   console.info(selectData)
+
+  // isotope query assembly from checkbox selections
+  let query = selectData.reduce((query, val) => {
+    query.push('.' + val[1].split(' ').join('-'))
+    return query
+  }, [])
+
+  filterGrid(query.join(','))
 })
 
 document
@@ -12,6 +27,16 @@ document
     checkbox.addEventListener('input', e => {
       const formData = new FormData(document.querySelector('form'))
       console.info(Array.from(formData.entries()))
+
+      // isotope query assembly from checkbox selections
+      let query = Array.from(formData.values()).reduce((query, val) => {
+        query.push('.' + val.split(' ').join('-'))
+        return query
+      }, [])
+
+      filterGrid(query.join(','))
     })  
   })
   
+const filterGrid = query =>
+  IsotopeGrid.arrange({filter: query})
