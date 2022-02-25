b1bfb195c1bf112d3e6d5614fc4bb97642be3be2
Adam Argyle <argyle@google.com>
2022-02-25 10:15:16 -0800

    use blingblingjs

--- progress/index.js
+++ progress/index.js
@@ -1,9 +1,11 @@
-const progress = document.querySelector('progress')
-const increase = document.querySelector('#increase')
-const decrease = document.querySelector('#decrease')
-const complete = document.querySelector('#complete')
-const reset    = document.querySelector('#reset')
-const zone     = document.querySelector('#loading-zone')
+import $ from 'blingblingjs'
+
+const progress = $('progress')
+const increase = $('#increase')
+const decrease = $('#decrease')
+const complete = $('#complete')
+const reset    = $('#reset')
+const zone     = $('#loading-zone')
 
 const state = {
   val: .3
@@ -11,13 +13,13 @@ const state = {
 
 const setProgress = () => {
   // set loading zone status
-  zone.setAttribute('aria-busy', state.val < 1)
+  zone.attr('aria-busy', state.val < 1)
 
   // clear attributes if no value to show
   // <progress> will show indeterminate state
   if (state.val === null) {
-    progress.removeAttribute('aria-valuenow')
-    progress.removeAttribute('value')
+    progress.attr('aria-valuenow', null)
+    progress.attr('value', null)
     return
   }
 
@@ -26,19 +28,19 @@ const setProgress = () => {
   const valPercent = val * 100 + "%"
   
   // set value for screenreaders and element values
-  progress.value = val
-  progress.setAttribute('aria-valuenow', valPercent)
-  progress.innerText = valPercent
+  progress[0].value = val
+  progress.attr('aria-valuenow', valPercent)
+  progress[0].innerText = valPercent
 
   // focus so screenreaders hear the announced value update
-  progress.focus()
+  progress[0].focus()
 }
 
 // on page load simulate partial completion
 setTimeout(_ => setProgress(), 2000)
 
 // DEMO EVENTS
-increase.addEventListener('click', e => {
+increase.on('click', e => {
   state.val += .2
   
   if (state.val > 1)
@@ -47,7 +49,7 @@ increase.addEventListener('click', e => {
   setProgress()
 })
 
-decrease.addEventListener('click', e => {
+decrease.on('click', e => {
   state.val -= .2
   
   if (state.val < 0)
@@ -56,12 +58,12 @@ decrease.addEventListener('click', e => {
   setProgress()
 })
 
-complete.addEventListener('click', e => {
+complete.on('click', e => {
   state.val = 1
   setProgress()
 })
 
-reset.addEventListener('click', e => {
+reset.on('click', e => {
   state.val = null
   setProgress()
 })
