27263086629089fb0ed379445b9ca35ce5e8eb4c
Adam Argyle <argyle@google.com>
2022-02-25 10:04:32 -0800

    adds verified screenreader ux, comments in the JS

--- progress/index.js
+++ progress/index.js
@@ -3,28 +3,48 @@ const increase = document.querySelector('#increase')
 const decrease = document.querySelector('#decrease')
 const complete = document.querySelector('#complete')
 const reset    = document.querySelector('#reset')
+const zone     = document.querySelector('#loading-zone')
 
 const state = {
   val: .3
 }
 
-const setProgress = val => {
-  if (val === null)
-    return progress.removeAttribute('value')
+const setProgress = () => {
+  // set loading zone status
+  zone.setAttribute('aria-busy', state.val < 1)
+
+  // clear attributes if no value to show
+  // <progress> will show indeterminate state
+  if (state.val === null) {
+    progress.removeAttribute('aria-valuenow')
+    progress.removeAttribute('value')
+    return
+  }
+
+  // round bad JS decimal math
+  const val = state.val.round(2)
+  const valPercent = val * 100 + "%"
   
+  // set value for screenreaders and element values
   progress.value = val
-  progress.innerText = val * 100 + "%"
+  progress.setAttribute('aria-valuenow', valPercent)
+  progress.innerText = valPercent
+
+  // focus so screenreaders hear the announced value update
+  progress.focus()
 }
 
-setTimeout(_ => setProgress(state.val), 2000)
+// on page load simulate partial completion
+setTimeout(_ => setProgress(), 2000)
 
+// DEMO EVENTS
 increase.addEventListener('click', e => {
   state.val += .2
   
   if (state.val > 1)
     state.val = 1
   
-  setProgress(state.val)
+  setProgress()
 })
 
 decrease.addEventListener('click', e => {
@@ -33,15 +53,19 @@ decrease.addEventListener('click', e => {
   if (state.val < 0)
     state.val = 0
   
-  setProgress(state.val)
+  setProgress()
 })
 
 complete.addEventListener('click', e => {
   state.val = 1
-  setProgress(state.val)
+  setProgress()
 })
 
 reset.addEventListener('click', e => {
   state.val = null
-  setProgress(state.val)
+  setProgress()
 })
+
+Number.prototype.round = function(places) {
+  return +(Math.round(this + "e+" + places)  + "e-" + places);
+}
