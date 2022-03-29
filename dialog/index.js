127b440713559aaf9665190dccead9571b8f27ef
Adam Argyle <argyle@google.com>
2022-03-29 14:20:55 -0700

    javascript api ready

--- dialog/index.js
+++ dialog/index.js
@@ -1,24 +1,81 @@
-// light dismiss
-opdialog.addEventListener('click', e => {
-  if (e.target.nodeName === 'DIALOG') {
-    opdialog.close('dismiss')
-  }
+// track opening
+const dialogAttrObserver = new MutationObserver((mutations, observer) => {
+  mutations.forEach(mutation => {
+    if (mutation.attributeName === 'open')
+      dialogOpen(mutation)
+  })
 })
 
-// dialog user input result
-opdialog.addEventListener('close', e => {
-  const userChoice = opdialog.returnValue
-  opdialog.querySelector('form')?.reset()
-
-  console.info(userChoice)
+// track deletion
+const dialogDeleteObserver = new MutationObserver((mutations, observer) => {
+  mutations.forEach(mutation => {
+    mutation.removedNodes.forEach(removedNode => {
+      if (removedNode.nodeName === 'DIALOG')
+        dialogDeleted(removedNode)
+    })
+  })
 })
 
-// prevent page load @keyframes scaling down the dialog
+// wait for all dialog animations to complete their promises
+const animationsComplete = element =>
+  Promise.allSettled(
+    element.getAnimations().map(animation => 
+      animation.finished))
+
+// click outside the dialog handler
+const lightDismiss = ({target:dialog}) => {
+  if (dialog.nodeName === 'DIALOG')
+    dialog.close('dismiss')
+}
+
+// dialog opening handler
+const dialogOpen = async ({target:dialog}) => {
+  const isOpen = dialog.hasAttribute('open')
+  if (!isOpen) return
+
+  console.log('Opening', dialog)
+  await animationsComplete(dialog)
+  console.log('Opened', dialog)
+}
+
+// dialog closing handler
+const dialogClose = async ({target:dialog}) => {
+  console.log('Closing', dialog)
+  console.info('Dialog user action:', dialog.returnValue)
+
+  const dialogFormData = new FormData(dialog.querySelector('form'))
+  console.info('Dialog form data', dialogFormData)
+
+  await animationsComplete(dialog)
+  console.log('Closed', dialog)
+
+  dialog.querySelector('form')?.reset()
+}
+
+// dialog deleted handler
+const dialogDeleted = dialog => {
+  dialog.removeEventListener('click', lightDismiss)
+  dialog.removeEventListener('close', dialogClose)
+  console.log('Dialog removed', dialog)
+}
+
+// page load dialogs setup
 document.querySelectorAll('dialog').forEach(async dialog => {
-  await Promise.allSettled(
-    dialog.getAnimations().map(animation => 
-      animation.finished
-    )
-  )
+  dialog.addEventListener('click', lightDismiss)
+  dialog.addEventListener('close', dialogClose)
+
+  dialogAttrObserver.observe(dialog, { 
+    attributes: true,
+  })
+
+  dialogDeleteObserver.observe(document.body, {
+    attributes: false,
+    subtree: false,
+    childList: true,
+  })
+
+  // remove loading attribute
+  // prevent page load @keyframes playing
+  await animationsComplete(dialog)
   dialog.removeAttribute('loading')
 })
