bc40f4550778cc5cb1e80fde049f5e91314ee82b
Adam Argyle <argyle@google.com>
2022-09-19 12:15:15 -0700

    enable :dir() polyfill for tooltips

--- tooltips/postcss.config.js
+++ tooltips/postcss.config.js
@@ -9,7 +9,6 @@ module.exports = {
         'prefers-color-scheme-query': false, 
         'gap-properties': false,
         'custom-properties': false,
-        'dir-pseudo-class': false,
         'is-pseudo-class': false,
         'focus-within-pseudo-class': false,
         'focus-visible-pseudo-class': false,
